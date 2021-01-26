c925dccd1c109a659a1e36505c0b3cfa735b2af7
Natalia <n.yarysheva@gmail.com>
2021-01-26 20:28:14 +0800

    before new Newmessage paradigm was implemented

--- src/ChatList/ChatList.js
+++ src/ChatList/ChatList.js
@@ -15,30 +15,33 @@ class ChatListComponent extends React.Component {
     if(this.props.chats.length > 0) {
         return (
             <div className='listOfChats'>
-                {
-                    this.props.chats.map((_chat, _index) => {
-                        return (
-                            <div key={_index}>
-                                <div className='chatListItem' id={_chat.messages[_chat.messages.length - 1].sender !== this.props.userEmail && this.props.newMessages ? 
-                                'unreadMessDetected' : 'allMessAreRead'} 
-                                onClick={() => this.select(_index)}
-                                selected={this.props.selectedChatIndex === _index}>
-                                    
-                                    <div className='avatar-circle'>
-                                        <h1 className='initials'>{_chat.users.find(friend => friend !== this.props.userEmail).split('')[0]}</h1>
-                                    </div>
-                                    
-                                    <div className='text'>
-                                        <p id='friendsEmail'>{_chat.users.filter(_user => _user !== this.props.userEmail)[0]}</p>
-                                        <br></br>
-                                        <p>"{_chat.messages[_chat.messages.length - 1].message.slice(0, 20)}..."</p>
+                <div className='chatsContainer'>
+                    {
+                        this.props.chats.map((_chat, _index) => {
+                            return (
+                                <div key={_index}>
+                                    <div className='chatListItem' id={_chat.messages[_chat.messages.length - 1].sender !== this.props.userEmail && this.props.newMessages ? 
+                                    'unreadMessDetected' : 'nonewmessages'} 
+                                    onClick={() => this.select(_index)}
+                                    selected={this.props.selectedChatIndex === _index}>
+                                        
+                                        <div className='avatar-circle'>
+                                            <h1 className='initials'>{_chat.users.find(friend => friend !== this.props.userEmail).split('')[0]}</h1>
+                                        </div>
+                                        
+                                        <div className='text'>
+                                            <p id='friendsEmail'>{_chat.users.filter(_user => _user !== this.props.userEmail)[0]}</p>
+                                            <br></br>
+                                            <p>"{_chat.messages[_chat.messages.length - 1].message.slice(0, 20)}..."</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                } 
-            </div>     
+                            )
+                        })
+                    } 
+            </div>
+            </div>
+                 
         );      
         } else {
             return (
