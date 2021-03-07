e219ec05ddd69a034ecbde9bf0a9d213f0b48939
Natalia <n.yarysheva@gmail.com>
2021-03-07 22:23:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,14 +12,7 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBi_NSBGYtRV1-bsmZMMfUfujthVEilXW4",
-  authDomain: "auth-81336.firebaseapp.com",
-  databaseURL: "https://auth-81336.firebaseio.com",
-  projectId: "auth-81336",
-  storageBucket: "auth-81336.appspot.com",
-  messagingSenderId: "89033781584",
-  appId: "1:89033781584:web:ac49f6c4936c2379e23c55",
-  measurementId: "G-NZV9ZFHFNQ"
+  ......
 });
 
 const routing = (
