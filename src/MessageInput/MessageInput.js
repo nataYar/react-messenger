cb2946b96ada29ee189cee92f7a01c57cba1359d
Natalia <n.yarysheva@gmail.com>
2021-03-28 12:30:57 +0800

    emojis can be added to chat

--- src/MessageInput/MessageInput.js
+++ src/MessageInput/MessageInput.js
@@ -1,21 +1,35 @@
 import React from "react";
 import MessageInput from './MessageInput.css';
-import 'tinymce/tinymce';
+import Picker from 'emoji-picker-react';
+import EmojiPicker from 'emoji-picker-react';
 
-import { Editor } from '@tinymce/tinymce-react';
+import JSEMOJI from 'emoji-js';
+import { EMSGSIZE, ENGINE_METHOD_DIGESTS } from "constants";
+import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
+// you can import it with a script tag instead
+
+// new instance
+const jsemoji = new JSEMOJI();
+
+// import 'tinymce/tinymce';
+// import { Editor } from '@tinymce/tinymce-react';
 
 class MessageInputComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            msgText: ''
+            msgText: '',
+            chosenEmoji: '',
+            showEmojis: false
         }
         this.onType = this.onType.bind(this);
         this.sendMsg = this.sendMsg.bind(this);
         this.textValidFn = this.textValidFn.bind(this);
         this.addDocFn = this.addDocFn.bind(this);
+        this.addEmoji = this.addEmoji.bind(this);
+        
     }
-   
+
     render () {
         if (this.props.visibility && this.props.selected != null ) {
             return (
@@ -23,8 +37,14 @@ class MessageInputComponent extends React.Component {
                     
                     <div className='txtInputContainerFlex'>
                         
-                        <textarea id='txtInput' placeholder="Write a message..." type="text"></textarea>
-                        <Editor  
+                        <textarea id='txtInput' placeholder="Write a message..." type="text"
+                        onKeyUp={e => this.onType(e)} >
+                        {/* onChange={e => this.addEmoji(e)} */}
+                        </textarea>
+                        <EmojiPicker onEmojiClick={this.addEmoji}/>
+                        {/* <Picker onEmojiClick={this.addEmoji} /> */}
+
+                        {/* <Editor  
                         apiKey="rlih7as5ochef82j5pprn4nrsqdu9egw9z3niba0uzuafqp0"
                         onKeyUp={e => this.onType(e)}
                         init={{
@@ -37,7 +57,7 @@ class MessageInputComponent extends React.Component {
                             statusbar: false,
                             selector: '#txtInput',
                             
-                        }}/>
+                        }}/> */}
  
 
                         <input type='file' className='icon attDoc' 
@@ -59,9 +79,20 @@ class MessageInputComponent extends React.Component {
         }
     }
 
-    // tinymce.init({
-        
-    // })
+    
+    addEmoji = (code, emoji) => {
+        console.log(emoji.unified);
+        // let emojiPic = jsemoji.replace_colons(`:${emoji.names[0]}:`);
+        let emojiPic = emoji.unified;
+        //fromCodePointFrom - decimal code point to string; parseInt  - grom hexadecimal to decimal code point
+        let emog = String.fromCodePoint(parseInt (emojiPic, 16));
+        console.log(emog);
+        this.setState({
+            msgText: this.state.msgText + `${emog}`
+        })
+        document.getElementById('txtInput').value = this.state.msgText;
+        console.log(this.state.msgText);
+    };
 
     // we set up message text to an input
     onType = (e) => e.keyCode === 13? this.sendMsg() : this.setState({msgText: e.target.value}); 
@@ -73,13 +104,15 @@ class MessageInputComponent extends React.Component {
     }
 
     sendMsg = () => {
-        console.log('clicked send');
         //checking if the message id valid
         if (this.textValidFn(this.state.msgText)) {
             //here should submit function from its parent(Dashboard)
             this.props.addMsgFn(this.state.msgText);
             //clear the input area
             document.getElementById('txtInput').value = '';
+            this.setState({
+                msgText: ''
+            })
         };
     };
     } 
